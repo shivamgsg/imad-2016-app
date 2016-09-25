5599d908ac2595093cce05be16043959ef8a4809
shivamgsg <shivamgupta.2015@vit.ac.in>
2016-09-26 02:01:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -82,12 +82,6 @@ app.get('/articleName',function(req,res) {
     res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/article-two',function(req,res) {
-     res.send(createTemplate(articletwo));
-});
-app.get('/article-three',function(req,res) {
-res.send(createTemplate(articlethree));
-});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
