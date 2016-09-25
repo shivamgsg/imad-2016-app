a8cdc7a38a347c7fe85ba081569674b58e3dca62
shivamgsg <shivamgupta.2015@vit.ac.in>
2016-09-26 02:44:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var ele = document.getElementById('1')
 ele.innerHTML='New'
+var img = document.getElementById('img');
+img.onclick = function()
+{
+    img.style.marginright='200px'
+}
