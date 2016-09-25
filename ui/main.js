0d586c8ab21ac7a92c292f3f2972d03c6531a915
shivamgsg <shivamgupta.2015@vit.ac.in>
2016-09-26 02:56:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,12 +3,12 @@ var ele = document.getElementById('1')
 ele.innerHTML='New'
 var img1 = document.getElementById('img1');
 var marginLeft=0;
-function moveRight()
-{marginLeft=marginLeft+10;
-img1.style.marginLeft=marginLeft +'px'; 
+function moveLeft()
+{marginRight=marginRight+10;
+img1.style.marginRight=marginRight +'px'; 
 }
 img1.onclick = function()
 {
    
-    var interval=setInterval(moveRight,100);
+    var interval=setInterval(moveLeft,30);
 }
